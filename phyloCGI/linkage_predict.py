63a327492735886f35f996e1821dd43fc51d4225
Yulong Niu <niuylscu@gmail.com>
2015-12-25 02:04:41 +0800

    update circos version to 0.69

--- phyloCGI/linkage_predict.py
+++ phyloCGI/linkage_predict.py
@@ -218,7 +218,7 @@ elif len(wm) == 0 and len(geneList) <= 7:
     r['writeConf']('phylo/', geneList, geneSym, linkColVec, fn + 'circosConfig/')
 
     ## circos plot
-    os.system('circos-0.67-7/bin/circos -conf ' + fn + 'circosConfig/circosConf.conf ' + '-outputdir ' + fn + ' -outputfile ' + 'circosPlot' + ' >/dev/null')
+    os.system('circos-0.69/bin/circos -conf ' + fn + 'circosConfig/circosConf.conf ' + '-outputdir ' + fn + ' -outputfile ' + 'circosPlot' + ' >/dev/null')
     os.system('convert -resize 700x700 ' + fn + 'circosPlot.png ' + fn + 'circosPlotWeb.png' + ' >/dev/null')
     circosFigObj = r['hwriteImage']('circosPlotWeb.png', center = True)
 
