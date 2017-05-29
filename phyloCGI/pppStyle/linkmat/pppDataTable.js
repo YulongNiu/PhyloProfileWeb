e3ac7c77b471c654c7f3282c2dfc5ba9c7b3456f
Yulong Niu <niuylscu@gmail.com>
2017-05-29 16:29:36 +0800

    move pppStyle into cgi folder

---
+++ phyloCGI/pppStyle/linkmat/pppDataTable.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var lastIdx = null;
+    var table = $('#linksmat').DataTable();
+
+    $('#linksmat tbody')
+        .on( 'mouseover', 'td', function () {
+            var colIdx = table.cell(this).index().column;
+
+            if ( colIdx !== lastIdx ) {
+                $( table.cells().nodes() ).removeClass( 'highlight' );
+                $( table.column( colIdx ).nodes() ).addClass( 'highlight' );
+            }
+        } )
+        .on( 'mouseleave', function () {
+            $( table.cells().nodes() ).removeClass( 'highlight' );
+        } );
+} );
