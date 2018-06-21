9c9cd70983b7e78dd3d5b11910c7f31ccc167019
Yulong Niu <niuylscu@gmail.com>
2018-06-21 22:02:40 +0800

    update pppSupplPy

---
+++ phyloCGI/pppSupplPy/README.md
@@ -0,0 +1,9 @@
+# Functions
+
+## `RandomName`
+
+`RandomName` is used to randomly generate a name. The length of file name will be set to 20.
+
+## `GetRFilePath`
+
+`GetRFilePath` is used to generate a R file path in python CGI.
--- phyloCGI/pppSupplPy/setup.py
+++ phyloCGI/pppSupplPy/setup.py
@@ -1,17 +1,21 @@
-from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(
     name = 'pppSupplPy',
-    version = '0.0.1',
+    version = '0.0.2',
     py_modules = ['pppSupplPy'],
 
     author = 'YulongNiu',
-    author_email = 'niuylscu@gmail.com',
+    author_email = 'yulong.niu@hotmail.com',
 
     url = 'http://yulongniu.bionutshell.org/',
-    description = 'A supplement python package for PrePhyloPro',
+    description = 'A supplement Python package for PrePhyloPro',
 
     packages = find_packages(),
+    
+     project_urls={
+        'Web application': 'http://bioinfor.scu.edu.cn/phyloprofile/',
+        'Source': 'https://github.com/YulongNiu/PhyloProfileWeb',
+    },
 )
 
